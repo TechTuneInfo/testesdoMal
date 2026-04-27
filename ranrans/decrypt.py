4e269535f673478e36bdbb8271da3e8ea9a4dded
TechTuneInfo <techtuneinformatica@gmail.com>
2026-04-27 09:59:00 -0300

    decrypt.py
    v0

--- ranrans/decrypt.py
+++ ranrans/decrypt.py
@@ -1,54 +1,32 @@
+from cryptography.fernet import Fernet
 import os
-from cryptography.fernet import Fernet, InvalidToken
 
-KEY_FILE = "key.key"
-TARGET_DIR = "test_files"
+def carregar_chave():
+    return open("chave.key", "rb").read()
 
-# Carrega a chave de decriptação do arquivo
-def load_key():
-    with open(KEY_FILE, "rb") as f:
-        return f.read()
+def descriptografar_arquivo(arquivo,chave):
+    f = Fernet(chave)
+    with open(arquivo, "rb") as file:
+        dados = file.read()
+        dados_descriptografados = f.decrypt(dados)
+    with open(arquivo, "wb") as file:
+        file.write(dados_descriptografados)
 
-# Decripta um arquivo
-def decrypt_file(filepath, key):
-    f = Fernet(key)
+def encontrar_arquivos(diretorio):
+    lista = []
+    for raiz, _, arquivos in os.walk(diretorio):
+        for nome in arquivos:
+            caminho = os.path.join(raiz, nome)
+            if nome != "ransoware.py" and not nome.endswith(".key"):
+                lista.append(caminho)
+    return lista 
 
-    with open(filepath, "rb") as file:
-        data = file.read()
-
-    try:
-        decrypted_data = f.decrypt(data)
-    except InvalidToken:
-        print(f"[!] Skipping (invalid key or file): {filepath}")
-        return
-
-    with open(filepath, "wb") as file:
-        file.write(decrypted_data)
-
-    print(f"[+] Decrypted: {filepath}")
-
-# Encontra arquivos alvo
-def find_files(directory):
-    files = []
-
-    for root, _, filenames in os.walk(directory):
-        for name in filenames:
-            path = os.path.join(root, name)
-
-            if not name.endswith(".key") and not name.endswith(".py"):
-                files.append(path)
-
-    return files
-
-# Execução principal (Main)
 def main():
-    key = load_key()
-    files = find_files(TARGET_DIR)
-
-    for file in files:
-        decrypt_file(file, key)
-
-    print("[+] Files successfully restored!")
+    chave = carregar_chave()
+    arquivos = encontrar_arquivos("test_files")
+    for arquivo in arquivos:
+        descriptografar_arquivo(arquivo, chave)
+    print("Arquivos restaurados com sucesso")
 
 if __name__ == "__main__":
     main()
